44e5c6fa6d0fade388ca9335ecfd97547f691068
HannahDwyer <dwyer.h@wustl.edu>
2026-03-30 10:53:17 -0500

    onPlay

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function Square({value, onSquareClick}){
   );
 }
 
-function Board(){
+function Board({xIsNext, squares, onPlay}){
 
   const [xIsNext, setXIsNext] = useState(true);
   const[squares, setSquares] = useState(Array(9).fill(null));
@@ -28,8 +28,7 @@ function Board(){
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
@@ -69,10 +68,15 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>{/*TODO*/}</ol>
