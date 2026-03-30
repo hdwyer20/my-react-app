321b02360d45a4c758a99d038de30afc8502859d
HannahDwyer <dwyer.h@wustl.edu>
2026-03-30 10:29:35 -0500

    added button with value to Square function

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,8 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 
-function Square(){
-  const[value, setValue] = useState(null);
-
-  function handleClick(){
-    setValue('X');
-  }
-
-  return (
-    <button className="square"
-    onClick = {handleClick}
-    >{value}</button>);
+function Square({value}){
+  return <button className="square">{value}</button>;
 }
 
 export default function Board(){
