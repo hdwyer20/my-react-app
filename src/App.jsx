550f9915a1fc4a6e374228ca04328f1acb168b97
HannahDwyer <dwyer.h@wustl.edu>
2026-03-30 10:59:42 -0500

    added function for going back to previous moves

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function Square({value, onSquareClick}){
 
 function Board({xIsNext, squares, onPlay}){
 
-  const [xIsNext, setXIsNext] = useState(true);
-  const[squares, setSquares] = useState(Array(9).fill(null));
+  //const [xIsNext, setXIsNext] = useState(true);
+  //const[squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -74,13 +74,31 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
